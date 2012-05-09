9b390fcd2da36bfddf00b3e97e9c46ed7fcdc28e
Chris Sund <jcanney@preferreditsystems.com>
2012-05-09 12:20:35 -0600

    try to fix asset issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,13 +15,12 @@ Annaco::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.precompile += %w( *.js *.css )
   config.assets.compile = true
   
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
